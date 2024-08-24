6b18af2a472a8d66f11111a04e32998f89c33c67
Zixian Jin <119010134@link.cuhk.edu.cn>
2024-08-24 17:21:54 +0800

    1. deleted all .dat files 2. fixed a bug in gelu_lut.sv

--- rtl/gelu_lut.sv
+++ rtl/gelu_lut.sv
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 module gelu_lut #(
-  .DATA_IN_0_PRECISION_0 = 8,
-  .DATA_IN_0_PRECISION_1 = 0,
-  .DATA_OUT_0_PRECISION_0 = 8,
-  .DATA_OUT_0_PRECISION_1 = 0
+  parameter DATA_IN_0_PRECISION_0 = 8,
+  parameter DATA_IN_0_PRECISION_1 = 0,
+  parameter DATA_OUT_0_PRECISION_0 = 8,
+  parameter DATA_OUT_0_PRECISION_1 = 0
 ) (
   input [DATA_IN_0_PRECISION_0 - 1 : 0] data_in_0,
   output [DATA_IN_0_PRECISION_0 - 1 : 0] data_out_0
